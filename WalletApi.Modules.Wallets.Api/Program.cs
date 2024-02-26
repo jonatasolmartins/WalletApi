ca29a17084c404bbefc7aa066cf4cf24c196a50f
jonatasolzirismartins <jonatasolmartins@outlook.com>
2024-02-26 00:25:07 -0300

    Added config file and secret manager to load the database and message bus config.

--- WalletApi.Modules.Wallets.Api/Program.cs
+++ WalletApi.Modules.Wallets.Api/Program.cs
@@ -1,7 +1,9 @@
+using ModularMonolith.Shared.Database;
 using WalletApi.Modules.Wallets.Api;
 using WalletApi.Modules.Wallets.Application;
 using Wolverine;
 using Wolverine.RabbitMQ;
+using Extensions = WalletApi.Modules.Wallets.Application.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +11,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
+builder.Services.Configure<PostgresConfig>(builder.Configuration.GetSection(PostgresConfig.ConfigName));
+
 builder.Host.UseWolverine(opts =>
 {
-    opts.UseRabbitMq(new Uri("amqps://"));
+    var rabbitMqSettings = builder.Configuration.GetSection(RabbitMqSettings.ConfigName).Get<RabbitMqSettings>();
+    
+    opts.UseRabbitMq(new Uri(rabbitMqSettings.ConnectionString));
     opts.Discovery.IncludeAssembly(typeof(Extensions).Assembly);
     
     opts.PublishAllMessages().ToRabbitQueue("wallet");
@@ -20,10 +26,12 @@ builder.Host.UseWolverine(opts =>
     opts.ListenToRabbitQueue("wallet");
 });
 
+
 builder.Services.AddHttpClient<UsersModuleApiService>(client =>
 {
     client.BaseAddress = new Uri("https://localhost:5227");
 });
+
 
 builder.Services.AddWalletsModule();
 
